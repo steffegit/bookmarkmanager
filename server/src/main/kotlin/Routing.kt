44eda0f82d24065c4cfb161e798f1e6a8984b731
steffegit <stefanga2020@gmail.com>
2025-07-10 17:42:40 +0300

    CRUD operations implemented

--- server/src/main/kotlin/Routing.kt
+++ server/src/main/kotlin/Routing.kt
@@ -1,19 +1,15 @@
 package me.atsteffe
 
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.html.*
-import io.ktor.server.http.content.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.html.*
 import me.atsteffe.model.Bookmark
+import me.atsteffe.util.BookmarkNotFoundException
 import me.atsteffe.util.DuplicateBookmarkUrlException
 import me.atsteffe.util.InvalidUrlException
-import org.jetbrains.exposed.sql.*
+import java.util.UUID
 
 
 fun Application.configureRouting() {
@@ -41,6 +37,66 @@ fun Application.configureRouting() {
                 call.respond(
                     HttpStatusCode.InternalServerError,
                     mapOf("error" to "Failed to create bookmark: ${e.message}")
+                )
+            }
+        }
+
+        put("/api/bookmarks/{id}") {
+            val requestId =
+                call.parameters["id"] ?: return@put call.respond(HttpStatusCode.BadRequest, "ID is required.")
+
+            val id = try {
+                UUID.fromString(requestId)
+            } catch (e: Exception) {
+                return@put call.respond(HttpStatusCode.BadRequest, "Invalid ID format.")
+            }
+
+            val bookmarkUpdate = call.receive<Bookmark>()
+
+            try {
+                val updatedBookmark = bookmarkService.updateBookmark(
+                    id,
+                    bookmarkUpdate.url,
+                    bookmarkUpdate.title,
+                    bookmarkUpdate.description
+                )
+                call.respond(HttpStatusCode.OK, updatedBookmark)
+            } catch (e: BookmarkNotFoundException) {
+                return@put call.respond(HttpStatusCode.NotFound, mapOf("error" to (e.message ?: "Bookmark not found")))
+            } catch (e: InvalidUrlException) {
+                return@put call.respond(HttpStatusCode.BadRequest, mapOf("error" to (e.message ?: "Invalid URL")))
+            } catch (e: Exception) {
+                call.respond(
+                    HttpStatusCode.InternalServerError,
+                    mapOf("error" to "Failed to update bookmark: ${e.message}")
+                )
+            }
+        }
+
+        delete("/api/bookmarks/{id}") {
+            val requestId =
+                call.parameters["id"] ?: return@delete call.respond(HttpStatusCode.BadRequest, "ID is required.")
+
+            val id = try {
+                UUID.fromString(requestId)
+            } catch (e: Exception) {
+                return@delete call.respond(HttpStatusCode.BadRequest, "Invalid ID format.")
+            }
+
+            try {
+                val isBookmarkDeleted: Boolean = bookmarkService.deleteBookmark(id)
+
+                if (isBookmarkDeleted) {
+                    call.respond(HttpStatusCode.NoContent, "Bookmark with ID $id successfully deleted.")
+                } else {
+                    call.respond(HttpStatusCode.NotFound, "Bookmark not found or could not be deleted.")
+                }
+            } catch (e: BookmarkNotFoundException) {
+                call.respond(HttpStatusCode.NotFound, mapOf("error" to (e.message ?: "Bookmark not found")))
+            } catch (e: Exception) {
+                call.respond(
+                    HttpStatusCode.InternalServerError,
+                    mapOf("error" to "Failed to delete bookmark: ${e.message}")
                 )
             }
         }
